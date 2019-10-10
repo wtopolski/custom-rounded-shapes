7b6b404181f8fd771ff13be8b81661e60961a1ea
Wojciech Topolski <wojciech.topolski@allegro.pl>
2019-10-10 09:51:38 +0200

    Add shapes

--- app/src/main/java/com/example/crs/MainActivity.kt
+++ app/src/main/java/com/example/crs/MainActivity.kt
@@ -16,6 +16,15 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         super.onCreate(savedInstanceState)
         findViewById<Button>(R.id.back).setOnClickListener { back() }
         findViewById<Button>(R.id.next).setOnClickListener { next() }
+
+        val navHost = findViewById<View>(R.id.nav_host_fragment).findNavController()
+        navHost.addOnDestinationChangedListener { _, _, _ ->
+            findViewById<Button>(R.id.next).visibility = if (navHost.currentDestination?.id == R.id.shape) {
+                View.GONE
+            } else {
+                View.VISIBLE
+            }
+        }
     }
 
     private fun next() {
@@ -23,16 +32,15 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         when (navHost.currentDestination?.id ?: 0) {
             R.id.material -> navHost.navigate(MaterialVariantFragmentDirections.actionMaterialToImage())
             R.id.image -> navHost.navigate(OutlineImageVariantFragmentDirections.actionImageToWidget())
+            R.id.widget -> navHost.navigate(OutlineWidgetVariantFragmentDirections.actionWidgetToShape())
             else -> { /* nop */ }
         }
     }
 
     private fun back() {
         val navHost = findViewById<View>(R.id.nav_host_fragment).findNavController()
-        when (navHost.currentDestination?.id ?: 0) {
-            R.id.image -> navHost.navigate(OutlineImageVariantFragmentDirections.actionImageToMaterial())
-            R.id.widget -> navHost.navigate(OutlineWidgetVariantFragmentDirections.actionWidgetToImage())
-            else -> { /* nop */ }
+        if (navHost.popBackStack().not()) {
+            finish()
         }
     }
 }
---
+++ app/src/main/java/com/example/crs/ShapeVariantFragment.kt
@@ -0,0 +1,5 @@
+package com.example.crs
+
+import androidx.fragment.app.Fragment
+
+class ShapeVariantFragment : Fragment(R.layout.fragment_shape_variant)
